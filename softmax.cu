97512b148aa2d246cc80b2f9a391f298f5b459f2
zytwd <1845371061@qq.com>
2026-03-18 23:04:43 +0800

    完成softmax的优化实现。

--- softmax.cu
+++ softmax.cu
@@ -85,15 +85,52 @@ __global__ void softmax_forward_naive(const float* input, float* output){
 //  1. 一个block负责计算4个rows。
 //  2. 引入float4向量化读取。
 //  3. 优化全局访存读取。
-template<int C, int rows_per_block>
+template<int C, int rows_per_block = 4>
 __global__ void softmax_forward_optimize(const float* input, float* output){
     // 计算线程信息，申明空间。
     // 一个block有4个wrap，每个wrap负责计算一个row。
-    const int row = blockIdx.x;
     const int thread_id = threadIdx.x;
     const int wrap_id = thread_id / WARP_SIZE;
     const int lane_id = thread_id % WARP_SIZE;
-    input += row * C;
+    const float4 *input_f4 = reinterpret_cast<const float4*>(input + (blockIdx.x * rows_per_block + wrap_id) * C);
+    float4 *output_f4 = reinterpret_cast<float4*>(output + (blockIdx.x * rows_per_block + wrap_id) * C);
+    constexpr int float4_per_thread = C / WARP_SIZE >> 2;
 
+    // -------|th 1|th 2|th 3|th 4|......--------------------------
+    // wrap 0:|----|----|----|----|......|----|
+    // wrap 1:|----|----|----|----|......|----|
+    // wrap 2:|----|----|----|----|......|----|
+    // wrap 3:|----|----|----|----|......|----|
+    // ------------------------------------------------------------
 
+    float4 reg_val_f4[float4_per_thread];
+    float max_val = -INFINITY;
+    for(int i = 0; i < float4_per_thread; ++i){
+        const float4 val = input_f4[i * WARP_SIZE + lane_id];
+        reg_val_f4[i] = val;
+        max_val = fmaxf(max_val, val.x);
+        max_val = fmaxf(max_val, val.y);
+        max_val = fmaxf(max_val, val.z);
+        max_val = fmaxf(max_val, val.w);
+    }
+    max_val = warpReduceMax(max_val);
+
+    float sum = 0.0f;
+    for(int i = 0; i < float4_per_thread; ++i){
+        reg_val_f4[i].x = expf(reg_val_f4[i].x - max_val);
+        reg_val_f4[i].y = expf(reg_val_f4[i].y - max_val);
+        reg_val_f4[i].z = expf(reg_val_f4[i].z - max_val);
+        reg_val_f4[i].w = expf(reg_val_f4[i].w - max_val);
+        sum += reg_val_f4[i].x + reg_val_f4[i].y + reg_val_f4[i].z + reg_val_f4[i].w;
+    }
+    sum = warpReduceSum(sum);
+
+    sum = 1.f / sum;
+    for(int i = 0; i < float4_per_thread; ++i){
+        reg_val_f4[i].x *= sum;
+        reg_val_f4[i].y *= sum;
+        reg_val_f4[i].z *= sum;
+        reg_val_f4[i].w *= sum;
+        output_f4[i * WARP_SIZE + lane_id] = reg_val_f4[i];
+    }
 }
