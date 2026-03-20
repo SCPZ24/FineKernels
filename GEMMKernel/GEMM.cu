5a1ebb8c4f12faddad6b07684f1cad16578bc83e
zytwd <1845371061@qq.com>
2026-03-21 00:06:27 +0800

    实现基础班转置算子。

---
+++ GEMMKernel/GEMM.cu
@@ -0,0 +1,8 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <iostream>
+
+template<int BM, int BN, int BK, int TM, int TN>
+__global__ void GEMM_naive(const float* A, const float* B, float* C, int M, int N, int K, const float alpha, const float beta){
+    
+}
